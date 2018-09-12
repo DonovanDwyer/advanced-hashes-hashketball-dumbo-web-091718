58bae0dbbcb827281b4a856c94837a712375b523
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-12 18:06:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -120,47 +120,48 @@ end
 def num_points_scored(player)
   play = player.split(" ")
   play = play.join("_").downcase
-  game_hash.each do |x, i|
-    i[:players].each do |y, e|
-      if play === y.to_s
-        return e[:points]
+  game_hash.each do |team, team_hash|
+    team_hash[:players].each do |name, stat_hash|
+      if name.to_s == play
+        return stat_hash[:points]
       end
     end
   end
 end
 
+
 def shoe_size(player)
   play = player.split(" ")
   play = play.join("_").downcase
-  game_hash.each do |x, i|
-    i[:players].each do |y, e|
-      if play === y.to_s
-        return e[:shoe]
+  game_hash.each do |team, team_hash|
+    team_hash[:players].each do |name, stat|
+      if play === name.to_s
+        return stat[:shoe]
       end
     end
   end
 end
 
 def team_colors(team)
-  game_hash.each do |x, i|
-    return i[:colors] if i[:team_name].to_s == team
+  game_hash.each do |t, team_stat|
+    return team_stat[:colors] if team_stat[:team_name].to_s == team
   end
 end
 
 def team_names
   arr = []
-  game_hash.each do |x, i|
-    arr << i[:team_name]
+  game_hash.each do |team, team_stat|
+    arr << team_stat[:team_name]
   end
   arr
 end
 
 def player_numbers(team)
   arr = []
-  game_hash.each do |x, i|
-    if i[:team_name].to_s == team
-      i[:players].each do |y, e|
-        arr << e[:number]
+  game_hash.each do |t, team_stat|
+    if team_stat[:team_name].to_s == team
+      team_stat[:players].each do |name, stat_hash|
+        arr << stat_hash[:number]
       end
     end
   end
@@ -170,25 +171,100 @@ end
 def player_stats(player)
   play = player.split(" ")
   play = play.join("_").downcase
-  game_hash.each do |x, i|
-    i[:players].each do |y, e|
-      if play === y.to_s
-        return e
+  game_hash.each do |team, team_stat|
+    team_stat[:players].each do |name, stat_hash|
+      if play === name.to_s
+        return stat_hash
       end
     end
   end
 end
 
+
+
+
 def big_shoe_rebounds
   max = 0
   mvp = nil
-  game_hash.each do |i, j|
-    j[:players].each do |k, l|
-      if l[:shoe] > max
-        max = l[:shoe]
-        mvp = l[:rebounds]
+  game_hash.each do |team, team_stat|
+    team_stat[:players].each do |name, stat_hash|
+      if stat_hash[:shoe] > max
+        max = stat_hash[:shoe]
+        mvp = stat_hash[:rebounds]
       end
     end
   end
   mvp
+end
+
+def most_points_scored
+  max = 0
+  mvp = nil
+  game_hash.each do |team, team_stat|
+    team_stat[:players].each do |name, stat_hash|
+      if stat_hash[:points] > max
+        max = stat_hash[:points]
+        mvp = name
+      end
+    end
+  end
+  return mvp
+end
+
+def winning_team
+  max = 0
+  mvt = nil
+  game_hash.each do |team, team_stat|
+    count = 0
+    team_stat[:players].each do |name, stat_hash|
+      count += stat_hash[:points]
+    end
+    if count > max
+      max = count
+      mvt = team_stat[:team_name]
+    end
+  end
+  return mvt
+end
+
+def player_with_longest_name
+  max = 0
+  mvp = nil
+  game_hash.each do |team, team_stats|
+    team_stats[:players].each do |name, stat_hash|
+      last_name = name.to_s.split("_")
+      last_name = last_name[1]
+      if last_name.length > max
+        max = last_name.length
+        mvp = name
+      end
+    end
+  end
+  mvp
+end
+
+def long_name_steals_a_ton?
+  max = 0
+  long_name = nil
+  game_hash.each do |team, team_stats|
+    team_stats[:players].each do |name, stat_hash|
+      last_name = name.to_s.split("_")
+      last_name = last_name[1]
+      if last_name.length > max
+        max = last_name.length
+        long_name = name
+      end
+    end
+  end
+  max_steal = 0
+  most_steals = nil
+  game_hash.each do |team, team_stats|
+    team_stats[:players].each do |name, stat_hash|
+      if stat_hash[:steals] > max_steal
+        max_steal = stat_hash[:steals]
+        most_steals = name
+      end
+    end
+  end
+  return most_steals == long_name
 end
